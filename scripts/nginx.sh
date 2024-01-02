dc1bea27a70b784d3d6a39b772a2deb9e5651be6
Vijay <vijay.thorat@biddano.com>
2024-01-03 01:18:36 +0530

    created pipeline for deployment

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,9 +4,9 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/EMandai/nginx/nginx.conf /etc/nginx/sites-available/cars
-sudo ln -s /etc/nginx/sites-available/cars /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/cars /etc/nginx/sites-enabled
+sudo cp /home/ubuntu/EMandai/nginx/nginx.conf /etc/nginx/sites-available/emandai
+sudo ln -s /etc/nginx/sites-available/emandai /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/emandai /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
