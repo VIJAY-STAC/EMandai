7662df46ec8e68dc07cfa7e164f4f63a421498bc
Vijay <vijay.thorat@biddano.com>
2024-01-07 01:09:44 +0530

    fix order count issue.

--- users/views.py
+++ users/views.py
@@ -88,7 +88,7 @@ class UserViewSet(viewsets.ModelViewSet):
                 'stat': True,
 	            'message': "Login successful",
                 'user_type': user.user_type,
-                'quadrant': user.quadrant.id if user.quadrant else None,
+                'quadrant': user.quadrant.id if user.quadrant else None
                 'quadrant_name': user.quadrant.name if user.quadrant else None,
                
                 
