6c44e6d4c8700b4a6ab53aa94049b8a42dddd212
Vijay <vijay.thorat@biddano.com>
2024-01-22 02:10:08 +0530

    created cart list api.

--- orders/serializers.py
+++ orders/serializers.py
@@ -48,6 +48,7 @@ class CartSerializer(serializers.ModelSerializer):
     product_name= serializers.CharField(source="product.product.name")
     packaging = serializers.CharField(source="product.product.packaging")
     product_images = serializers.SerializerMethodField(default=None)
+    quantity = serializers.CharField(source="product.discount", default=0)
     class Meta:
         model = Cart
         fields =(
@@ -57,9 +58,11 @@ class CartSerializer(serializers.ModelSerializer):
             "amt",
             "product_name",
             "packaging",
-            "product_images"
+            "product_images",
+            "quantity",
         )
 
     def get_product_images(self, obj):
         img = obj.product.product.product_images.all().first().url
         return img
+
